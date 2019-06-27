a54030573df670e32b7555d3a2517e83bed08407
Vatyam Sai Yashasvi Kumar <44964137+yashasvik98@users.noreply.github.com>
2019-06-27 23:49:53 +0530

    Image uploading module done.

--- server.js
+++ server.js
@@ -1,8 +1,50 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const multer = require("multer");
+const ejs = require("ejs");
+const path = require("path");
+
+// Set Storage Engine
+const storage = multer.diskStorage({
+  destination: "./public/uploads/",
+  filename: function(req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  }
+});
+// Init Upload
+const upload = multer({
+  storage: storage,
+  fileFilter: function(req, file, cb) {
+    checkFileType(file, cb);
+  }
+}).single("myImage");
+
+// Check File Type
+function checkFileType(file, cb) {
+  // Allowed extensions
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check extensions
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images only!");
+  }
+}
 
 const app = express();
+
+// EJS
+app.set("view engine", "ejs");
+
+// Public Folder
+app.use(express.static("./public"));
 
 // Bodyparser Middleware
 app.use(bodyParser.json());
@@ -15,6 +57,29 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB connected..."))
   .catch(err => console.log(err));
+
+// Routes
+app.get("/", (req, res) => res.render("index"));
+app.post("/upload", (req, res) => {
+  upload(req, res, err => {
+    if (err) {
+      res.render("index", {
+        msg: err
+      });
+    } else {
+      if (req.file == undefined) {
+        res.render("index", {
+          msg: "Error: No File selected!"
+        });
+      } else {
+        res.render("index", {
+          msg: "File uploaded!",
+          file: `uploads/${req.file.filename}`
+        });
+      }
+    }
+  });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
