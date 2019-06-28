9d48b1232959d5ca17881f2f8d7b4d63931215b3
Vatyam Sai Yashasvi Kumar <44964137+yashasvik98@users.noreply.github.com>
2019-06-28 15:51:46 +0530

    Changed upload file location from server to client.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 // Set Storage Engine
 const storage = multer.diskStorage({
-  destination: "./public/uploads/",
+  destination: "./client/public/uploads/",
   filename: function(req, file, cb) {
     cb(
       null,
